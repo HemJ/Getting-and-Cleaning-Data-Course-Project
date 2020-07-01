32ffda5b8856b8417c944be7277fac318c763393
Hemant Jagtap <jagtaphem@gmail.com>
2020-07-01 20:22:09 +0530

    Updated run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -27,13 +27,13 @@ features      <- read.table("UCI HAR Dataset/features.txt", col.names = c("featu
 
 # read test data
 subject_test  <- read.table("UCI HAR Dataset/test/subject_test.txt", col.names = "subjectId")
-x_test      <- read.table("UCI HAR Dataset/test/X_test.txt", col.names = features$featureLabel)
-y_test      <- read.table("UCI HAR Dataset/test/y_test.txt", col.names = "activityId")
+x_test        <- read.table("UCI HAR Dataset/test/X_test.txt", col.names = features$featureLabel)
+y_test        <- read.table("UCI HAR Dataset/test/y_test.txt", col.names = "activityId")
 
 # read training data
 subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt", col.names = "subjectId")
-x_train     <- read.table("UCI HAR Dataset/train/X_train.txt", col.names = features$featureLabel)
-y_train     <- read.table("UCI HAR Dataset/train/y_train.txt", col.names = "activityId")
+x_train       <- read.table("UCI HAR Dataset/train/X_train.txt", col.names = features$featureLabel)
+y_train       <- read.table("UCI HAR Dataset/train/y_train.txt", col.names = "activityId")
 
 #-------------------------------------------------------------------------------
 
